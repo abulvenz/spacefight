0618962ee60ac9a88998146de34ec19fee531bd3
Andreas Eismann <abulvenz@gmail.com>
2019-02-03 22:22:51 +0100

    First working version with all the fanciness needed.

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,12 @@
 import m from "mithril";
 import tagl from "tagl-mithril";
 import fn from "./fn";
+import images from "images/*.png";
 
+// `ex${('00'.substr(0,2-String(p.burn()).length))+p.burn()}`
+let exid = n => `#ex${"00".substring(0, 2 - String(n).length) + String(n)}`;
+
+console.log(exid(53));
 const {
   button,
   div,
@@ -14,8 +19,11 @@ const {
   textarea,
   h1,
   text,
+  img,
   hr,
-  line
+  line,
+  small,
+  foreignObject
 } = tagl(m);
 
 const Options = Object.freeze({
@@ -40,14 +48,22 @@ const Player = color => {
     y > innerHeight && (y -= innerHeight);
   };
   const shoot = opp => {
-    let ex = Math.cos((angle / 180) * Math.PI);
-    let ey = Math.sin((angle / 180) * Math.PI);
+    let e1x = Math.cos((angle / 180) * Math.PI);
+    let e1y = Math.sin((angle / 180) * Math.PI);
+    let e2x = Math.cos(((angle + 90) / 180) * Math.PI);
+    let e2y = Math.sin(((angle + 90) / 180) * Math.PI);
     let dx = opp.x() - x;
     let dy = opp.y() - y;
     let d = Math.sqrt(dx * dx + dy * dy);
-    let s = (ex * dx + ey * dy) / Math.max(d, 1);
-    line({x1:x+ex*v,y1:y+ey*v,x2:x+ex*1000,y2:y+ey*1000});
-    if (s > 0.99) {
+    let s1 = e1x * dx + e1y * dy; // / Math.max(d, 1);
+    let s2 = e2x * dx + e2y * dy; // / Math.max(d, 1);
+    line({
+      x1: x + e1x * v,
+      y1: y + e1y * v,
+      x2: x + e1x * 1000,
+      y2: y + e1y * 1000
+    });
+    if (s1 > 0 && Math.abs(s2) < 5 && d < 1000) {
       opp.explode();
     }
   };
@@ -58,20 +74,35 @@ const Player = color => {
     angle -= 5;
   };
 
-  let neutralize = () =>{return { x1: 0, y1: 0, x2: 0, y2: 0 }};
+  let neutralize = () => {
+    return { x1: 0, y1: 0, x2: 0, y2: 0 };
+  };
 
   let shot = neutralize();
   let line = ({ x1, y1, x2, y2 }) => (shot = { x1, y1, x2, y2 });
+
+  let burn = -1;
 
   return {
     color: () => color,
     x: () => x,
     y: () => y,
-    explode: () => (v = 0),
+    v: () => v,
+    explode: () => {
+      v = 0;
+      burn = 0;
+      console.log("I have been hit!");
+    },
     angle: () => angle,
     line: () => shot,
+    burn: () => burn,
     next: (option, opp) => {
-      neutralize();
+      shot = neutralize();
+      if (burn > -1) {
+        burn++;
+        // burn = 2;
+        return;
+      }
       switch (option) {
         case Options.TURN_LEFT:
           turnLeft();
@@ -104,26 +135,43 @@ const PlayerView = vnode => {
     view(vnode) {
       let p = vnode.attrs.player;
       let l = p.line();
-      return [g(
-        { transform: `translate(${p.x()},${p.y()})rotate(${p.angle()})` },
-        rect({
-          x: -5,
-          y: -5,
-          width: 10,
-          height: 10,
-          fill: p.color(),
-          stroke: p.color()
-        }),        
-      ),
-        line({...l,style:"stroke:rgb(255,0,0);stroke-width:2",})
-    ];
+      return [
+        g(
+          { transform: `translate(${p.x()},${p.y()})rotate(${p.angle()})` },
+          rect({
+            x: -5,
+            y: -5,
+            width: 10,
+            height: 10,
+            fill: p.color(),
+            stroke: p.color()
+          }),
+          // p.burn()>0?
+          foreignObject(
+            {
+              x: -32,
+              y: -32,
+              width: 64,
+              height: 64,
+              transform: "translate(0, 0)"
+            },
+            //            { x: -32, y: -32, width: 64, height: 64 },
+            div(
+              { xmlns: "http://www.w3.org/1999/xhtml" },
+              img[exid(p.burn())]({ src: images.trans })
+            )
+          ) //:null
+        ),
+        line({ ...l, style: "stroke:rgb(255,0,0);stroke-width:2" })
+      ];
     }
   };
 };
 
 const PlayerData = () => {
   return {
-    programText: "return ()=>Math.random()<.3?Options.SHOOT:Math.random()<.5?Options.TURN_LEFT:Options.TURN_RIGHT",
+    programText:
+      "return (p,o)=>Math.random()<.3?Options.SHOOT:Math.random()<.5?Options.TURN_LEFT:Options.TURN_RIGHT",
     color: "green",
     name: ""
   };
@@ -142,12 +190,16 @@ const GameService = () => {
       player2.f = new Function("Options", player2.programText)(Options);
     },
     run() {
+      if (!started) {
+        p1 = Player("#f233f3");
+        p2 = Player("#109912");
+      }
       started = true;
       setTimeout(() => {
         time += step;
         p1.next(player1.f(p1, p2, Options), p2);
         p2.next(player2.f(p2, p1, Options), p1);
-        if (time > 15000) {
+        if (time > 1500000) {
           started = false;
           time = 0;
         } else game.run();
@@ -165,8 +217,8 @@ const GameView = () => {
         svg(
           { width: innerWidth - 10, height: innerHeight - 10 },
           m(PlayerView, { player: p1 }),
-          m(PlayerView, { player: p2 }),
-          text({ x: 10, y: 10, stroke: "black" }, "state: " + state)
+          m(PlayerView, { player: p2 })
+          //   text({ x: 10, y: 10, stroke: "black" }, "state: " + state)
         )
       );
     }
@@ -206,7 +258,7 @@ const StartView = () => {
                 game.run();
               }
             },
-            "START"
+            small("START")
           )
         ),
         hr(),
